4acf6b03caf9e7441afb5e2543530054b1bf3eeb
Dominique Kornegay <dominiquekornegay@gmail.com>
2022-05-25 18:57:17 -0400

    commit tree 1a02fba71d7c0de1c5bda1d3fbe9781c57c2dc42

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = batteryBatches.reduce(function(batteries, factoryTotal) {
+    factoryTotal += batteries
+    return factoryTotal
+}, 0)
